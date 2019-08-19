513ecb37796c0c9444aea5cf6364f981a256db65
hsankesara <heetsankesara3@gmail.com>
2019-08-19 17:23:43 +0530

    Added FIRE registration example in the main file

--- main.py
+++ main.py
@@ -5,8 +5,19 @@ import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.utils import data
 from torch.autograd import Variable
 import numpy as np
+import matplotlib.pyplot as plt
+import skimage.io as io
+import os
+from skimage.transform import resize
+import multiprocessing as mp
+from tqdm import tqdm
+import gc
+import time
+from sklearn.model_selection import train_test_split
+from matplotlib.lines import Line2D
 use_gpu = torch.cuda.is_available()
 
 
@@ -61,7 +72,7 @@ class VoxelMorph():
             return train_loss, train_dice_score
         return train_loss
 
-    def get_training_loss(self, batch_moving, batch_fixed, n=9, lamda=0.01):
+    def get_test_loss(self, batch_moving, batch_fixed, n=9, lamda=0.01):
         with torch.set_grad_enabled(False):
             registered_image = self.voxelmorph(batch_moving, batch_fixed)
             val_loss = self.vm.vox_morph_loss(
@@ -70,13 +81,78 @@ class VoxelMorph():
             return val_loss, val_dice_score
 
 
+class Dataset(data.Dataset):
+    'Characterizes a dataset for PyTorch'
+
+    def __init__(self, list_IDs):
+        'Initialization'
+        self.list_IDs = list_IDs
+
+    def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.list_IDs)
+
+    def __getitem__(self, index):
+        'Generates one sample of data'
+        # Select sample
+        ID = self.list_IDs[index]
+
+        # Load data and get label
+        fixed_image = torch.Tensor(
+            resize(io.imread('../input/fire/Images/' + ID + '_1.jpg'), (1024, 1024, 3)))
+        moving_image = torch.Tensor(
+            resize(io.imread('../input/fire/Images/' + ID + '_2.jpg'), (1024, 1024, 3)))
+        return fixed_image, moving_image
+
+
 def main():
     '''
     In this I'll take example of FIRE: Fundus Image Registration Dataset
     to demostrate the working of the API.
     '''
     vm = VoxelMorph((1, 128, 128, 128))
-    DATA_PATH = 'PATH TO THE DATASET'
+    DATA_PATH = './fire-fundus-image-registration-dataset/'
+    params = {'batch_size': 3,
+              'shuffle': True,
+              'num_workers': 6,
+              'worker_init_fn': np.random.seed(42)
+              }
+
+    max_epochs = 50
+    filename = list(set([x.split('_')[0]
+                         for x in os.listdir('../input/fire/Images/')]))
+    partition = {}
+    partition['train'], partition['validation'] = train_test_split(
+        filename, test_size=0.33, random_state=42)
+
+    # Generators
+    training_set = Dataset(partition['train'])
+    training_generator = data.DataLoader(training_set, **params)
+
+    validation_set = Dataset(partition['validation'])
+    validation_generator = data.DataLoader(validation_set, **params)
+    # Loop over epochs
+    for epoch in range(max_epochs):
+        start_time = time.time()
+        train_loss = 0
+        train_dice_score = 0
+        val_loss = 0
+        val_dice_score = 0
+        for batch_fixed, batch_moving in training_generator:
+            loss, dice = vm.train_model(batch_moving, batch_fixed)
+            train_dice_score += dice.data
+            train_loss += loss.data
+        print('[', "{0:.2f}".format((time.time() - start_time) / 60), 'mins]', 'After', epoch + 1, 'epochs, the Average training loss is ', train_loss *
+              params['batch_size'] / len(training_set), 'and average DICE score is', train_dice_score.data * params['batch_size'] / len(training_set))
+        # Testing time
+        start_time = time.time()
+        for batch_fixed, batch_moving in validation_generator:
+            # Transfer to GPU
+            loss, dice = vm.get_test_loss(batch_moving, batch_fixed)
+            val_dice_score += dice.data
+            val_loss += loss.data
+        print('[', "{0:.2f}".format((time.time() - start_time) / 60), 'mins]', 'After', epoch + 1, 'epochs, the Average validations loss is ', val_loss *
+              params['batch_size'] / len(validation_set), 'and average DICE score is', val_dice_score.data * params['batch_size'] / len(validation_set))
 
 
 if __name__ == "__main__":
